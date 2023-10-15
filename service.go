b8ae79bc1e393689b39727110a2323287afbc151
flowerinthenight <flowerinthenight@pm.me>
2023-10-16 04:39:56 +0900

    chore: wip: add broadcast to svc

--- service.go
+++ service.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	"github.com/alphauslabs/juno/internal/fleet"
 	v1 "github.com/alphauslabs/juno/proto/v1"
@@ -14,7 +15,6 @@ import (
 
 type service struct {
 	fd *fleet.FleetData
-
 	v1.UnimplementedJunoServer
 }
 
@@ -22,11 +22,12 @@ func (s *service) Lock(in *v1.LockRequest, stream v1.Juno_LockServer) error {
 	return status.Errorf(codes.Unimplemented, "method Lock not implemented")
 }
 
-func (s *service) Unlock(ctx context.Context, in *v1.UnlockRequest) (*v1.UnlockResponse, error) {
+func (s *service) Unlock(ctx context.Context, req *v1.UnlockRequest) (*v1.UnlockResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Unlock not implemented")
 }
 
-func (s *service) AddToSet(ctx context.Context, in *v1.AddToSetRequest) (*v1.AddToSetResponse, error) {
+func (s *service) AddToSet(ctx context.Context, req *v1.AddToSetRequest) (*v1.AddToSetResponse, error) {
+	defer func(begin time.Time) { glog.Infof("method AddToSet took %v", time.Since(begin)) }(time.Now())
 	ch := make(chan hedge.BroadcastOutput)
 	go s.fd.App.FleetOp.Broadcast(ctx, []byte("hello"), hedge.BroadcastArgs{Out: ch})
 	for v := range ch {
